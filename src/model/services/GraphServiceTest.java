f07da9e0dd017727d217742f6a6f362aadac7b71
Alejandro <correa92.dev@gmail.com>
2025-05-05 00:36:38 -0300

    se suma test

--- src/model/services/GraphServiceTest.java
+++ src/model/services/GraphServiceTest.java
@@ -39,30 +39,25 @@ public class GraphServiceTest {
 		GraphService graph = new GraphService();
 		assertFalse(graph.existVertex(null));
 	}
-
-	@Test
-	public void validateVertex_WithNullVertex_ThrowsException() {
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void existeVertice_conDosVerticeIguales_ThrowsException() {
 		GraphService graph = new GraphService();
-
-		try {
-			graph.validateVertex(null);
-			fail("Se esperaba IllegalArgumentException");
-		} catch (IllegalArgumentException e) {
-			assertEquals("El vértice no puede ser nulo.", e.getMessage());
-		}
+        graph.addVertex("A");
+        graph.addVertex("A");
 	}
 
-	@Test
+	@Test(expected = IllegalArgumentException.class)
+	public void validateVertex_WithNullVertex_ThrowsException() {
+		GraphService graph = new GraphService();
+		graph.validateVertex(null);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
 	public void validateVertex_WithNonExistentVertex_ThrowsException() {
 		GraphService graph = new GraphService();
 		Vertex v = new Vertex(0, "X");
-
-		try {
-			graph.validateVertex(v);
-			fail("Se esperaba IllegalArgumentException");
-		} catch (IllegalArgumentException e) {
-			assertTrue(e.getMessage().contains("X"));
-		}
+		graph.validateVertex(v);
 	}
 
 	@Test
